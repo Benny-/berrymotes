4f377c963623055008d5cdddcd486f9e1bbcc9be
Benny <Benny@gmx.it>
2014-11-17 19:34:27 +0100

    Disabled dynamicFinders for performance

--- config/models.js
+++ config/models.js
@@ -28,5 +28,5 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
   migrate: 'alter', // Options: 'safe' 'alter' 'drop'
-
+  dynamicFinders: false, // Disabled for performance reasons.
 };
