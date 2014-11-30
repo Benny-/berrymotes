a61a74c094fe129326d4ba43f13ba7c564756d3d
Benny <Benny@gmx.it>
2014-11-30 13:13:05 +0100

    A 403 response will not hide the reason while in production mode

--- api/responses/forbidden.js
+++ api/responses/forbidden.js
@@ -26,13 +26,8 @@ module.exports = function forbidden (data, options) {
   if (data !== undefined) {
     sails.log.verbose('Sending 403 ("Forbidden") response: \n',data);
   }
-  else sails.log.verbose('Sending 403 ("Forbidden") response');
-
-  // Only include errors in response if application environment
-  // is not set to 'production'.  In production, we shouldn't
-  // send back any identifying information about errors.
-  if (sails.config.environment === 'production') {
-    data = undefined;
+  else {
+    sails.log.verbose('Sending 403 ("Forbidden") response');
   }
 
   // If the user-agent wants JSON, always respond with JSON
